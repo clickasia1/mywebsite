011fb72b9e378b9a8ad69e483c5403212974963e
clickasia1 <72312767+clickasia1@users.noreply.github.com>
2020-10-10 13:06:01 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Raloi</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@500&display=swap" rel="stylesheet">
+  
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">Raloi</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          My Men
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Clothes</a>
+          <a class="dropdown-item" href="#">Bags</a>
+          <a class="dropdown-item" href="#">Shoes</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Accessories</a>
+        </div>
+      </li>
+<li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Female
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Clothes</a>
+          <a class="dropdown-item" href="#">Bags</a>
+          <a class="dropdown-item" href="#">Shoes</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Jewelery</a>
+        </div>
+      </li>
+
+<li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Children And Infants
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Children</a>
+          <a class="dropdown-item" href="#">Daughters</a>
+          <a class="dropdown-item" href="#">Infants (0-24 Months)</a>
+          
+        </div>
+      </li>
+<div class="dropdown-divider"></div>
+<li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Who We Are
+        </a>
+        </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+
+<div class="jumbotron">
+  <h1>About Raloi</h1>
+  <p>100% Saudi company, we shop for you from around the world for the purpose of delivering global products to your door at a price that competes with everyone, and all of our products are from their original source</p>
+</div>
+
+<section class="My-5">
+  <div class="py-5">
+   <h2 class="text-center">About Us</h2> 
+  </div>
+  <div class="container-Fluid">
+  <div class="row">
+    <div class="col-lg-6 col-md-6 col-12">
+        <img src="image/image/img5.webp" class="img-fluid aboutimg"> 
+    </div>
+    <div class="col-lg-6 col-md-6 col-12">
+     <h2 class="display-4">RECENTLY ARRIVED</h2> 
+     <p class="py-3">A 100% Saudi company, we shop for you from around the world for the purpose of delivering global products to your door at a price that competes with everyone, and all of our products are from their original source</p>
+     <a href="about.php" class="btn btn-success">Click More</a>  
+      </div>
+    </div>
+  </div>
+
+</section>
+
+<footer>
+  <p class="p-3 bg-light text-black text-center">
+    Raloi
+Copyright © 2020 All rights reserved
+  </p> 
+ </footer>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Raloi</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@500&display=swap" rel="stylesheet">
+  
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">Raloi</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          My Men
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Clothes</a>
+          <a class="dropdown-item" href="#">Bags</a>
+          <a class="dropdown-item" href="#">Shoes</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Accessories</a>
+        </div>
+      </li>
+<li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Female
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Clothes</a>
+          <a class="dropdown-item" href="#">Bags</a>
+          <a class="dropdown-item" href="#">Shoes</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Jewelery</a>
+        </div>
+      </li>
+
+<li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Children And Infants
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Children</a>
+          <a class="dropdown-item" href="#">Daughters</a>
+          <a class="dropdown-item" href="#">Infants (0-24 Months)</a>
+          
+        </div>
+      </li>
+<div class="dropdown-divider"></div>
+<li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Who We Are
+        </a>
+        </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image/img-home.jpg" alt="Raloi" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Raloi</h3>
+        <p>A big online Shopping Company!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="image/img-01.jpg" alt="Raloi" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Raloi Main</h3>
+        <p>
+          A 100% Saudi company, we shop for you from around the world!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="image/img-05.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Raloi View</h3>
+        <p>We love Our Customer!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<section class="My-5">
+  <div class="py-5">
+   <h2 class="text-center">About Us</h2> 
+
+  </div>
+  <div class="container-Fluid">
+  <div class="row">
+    <div class="col-lg-6 col-md-6 col-12">
+        <img src="image/img5.webp" class="img-fluid aboutimg"> 
+    </div>
+    <div class="col-lg-6 col-md-6 col-12">
+     <h2 class="display-4">RECENTLY ARRIVED</h2> 
+     <p class="py-3">A 100% Saudi company, we shop for you from around the world for the purpose of delivering global products to your door at a price that competes with everyone, and all of our products are from their original source</p>
+     <a href="about.php" class="btn btn-success">Click More</a>  
+      </div>
+    </div>
+  </div>
+
+</section>
+
+<section class="My-5">
+  <div class="py-5">
+   <h2 class="text-center">Our Product</h2> 
+   </div>
+     
+   <div class="container-Fluid">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12">
+        <div class="card" >
+  <img class="card-img-top" src="image/logo.webp" alt="Card image">
+        <div class="card-body">
+          <h4 class="card-title">Raloi</h4>
+          <p class="card-text">Raloi Product.</p>
+          <a href="#" class="btn btn-primary">See Details</a>
+        </div>
+      </div>
+      </div>
+
+
+<div class="col-lg-4 col-md-4 col-12">
+        <div class="card" >
+  <img class="card-img-top" src="image/logo.webp" alt="Card image">
+        <div class="card-body">
+          <h4 class="card-title">Raloi</h4>
+          <p class="card-text">Raloi Product.</p>
+          <a href="#" class="btn btn-primary">See Details</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-4 col-12">
+        <div class="card" >
+  <img class="card-img-top" src="image/logo.webp" alt="Card image">
+        <div class="card-body">
+          <h4 class="card-title">Raloi</h4>
+          <p class="card-text">Raloi Product.</p>
+          <a href="#" class="btn btn-primary">See Details</a>
+        </div>
+</div>
+  </div>
+    </div>
+      </div>
+      </section>
+
+<section class="My-5">
+  <div class="py-5">
+   <h2 class="text-center">Product Gallery</h2> 
+   </div> 
+
+<div class="container-Fluid">
+  <div class="row">
+    <div class="col-lg-4 col-md-4 col-12">
+<img src="image/logo.webp" class="img-fluid pb-5">
+    </div>
+    <div class="col-lg-4 col-md-4 col-12">
+<img src="image/logo.webp" class="img-fluid pb-5">
+    </div>
+    <div class="col-lg-4 col-md-4 col-12">
+<img src="image/logo.webp" class="img-fluid pb-5">
+    </div>
+    <div class="col-lg-4 col-md-4 col-12">
+<img src="image/logo.webp" class="img-fluid pb-5">
+    </div>
+    <div class="col-lg-4 col-md-4 col-12">
+<img src="image/logo.webp" class="img-fluid pb-5">
+    </div>
+    <div class="col-lg-4 col-md-4 col-12">
+<img src="image/logo.webp" class="img-fluid pb-5">
+    </div>
+    <div class="col-lg-4 col-md-4 col-12">
+<img src="image/logo.webp" class="img-fluid pb-5">
+    </div>
+    <div class="col-lg-4 col-md-4 col-12">
+<img src="image/logo.webp" class="img-fluid pb-5">
+    </div>
+    <div class="col-lg-4 col-md-4 col-12">
+<img src="image/logo.webp" class="img-fluid pb-5">
+    </div>
+  </div>
+  
+</div>
+
+ </section>
+
+
+
+<section class="My-5">
+  <div class="py-5">
+   <h2 class="text-center">Contact Us</h2> 
+   </div> 
+
+   <div class="w-50 m-auto">
+     <form action="user.php" method="post">
+     <div class="form-group">
+        <label>Username</label> 
+        <input type="text" name="user" autocomplete="off" class="form-control">
+     </div>  
+     <div class="form-group">
+        <label>Eamil ID</label> 
+        <input type="text" name="email" autocomplete="off" class="form-control">
+     </div>  
+     <div class="form-group">
+        <label>Mobile Number</label> 
+        <input type="text" name="mobile" autocomplete="off" class="form-control">
+     </div> 
+     <div class="form-group">
+        <label>Comments</label> 
+        <textarea class="form-control" name="comments">
+          
+        </textarea>
+     </div>  
+     <button type="submit" class="btn btn-success">Submit</button> 
+     </form>
+   </div>
+ </section>
+
+ <footer>
+  <p class="p-3 bg-light text-black text-center">
+    Raloi
+Copyright © 2020 All rights reserved
+  </p> 
+ </footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
